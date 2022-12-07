7512e678bc1c990deac1df8c4f4866069f012f5a
Karl <knord2112@gmail.com>
2022-12-07 12:15:46 -0700

    Centered Image

--- missingPersons/home/views.py
+++ missingPersons/home/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 def indexPageView(request):
     return render(request, 'homepages/index.html')
 
+
 def HomePageView(request):
     return render(request, 'homepages/homepage.html')
-
-def searchPageView(request):
-    return render(request, 'homepages/search.html')
