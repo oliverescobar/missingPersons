f71f3337ae06bc8a33d8a0f69134f888beae9db4
zolsen10 <soccerzack10@yahoo.com>
2022-12-07 12:56:31 -0700

    Added the searchPageView to views

--- missingPersons/home/views.py
+++ missingPersons/home/views.py
@@ -7,3 +7,7 @@ def indexPageView(request):
 
 def HomePageView(request):
     return render(request, 'homepages/homepage.html')
+
+
+def searchPageView(request):
+    return render(request, 'homepages/search.html')
