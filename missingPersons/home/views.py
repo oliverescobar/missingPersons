2b41308ecbf594a386aadab32a9d6f30ce99ad55
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2022-12-07 21:21:13 -0700

    pls don't delete this

--- missingPersons/home/views.py
+++ missingPersons/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import missingPerson
 
 
 def indexPageView(request):
@@ -10,7 +11,18 @@ def HomePageView(request):
 
 
 def searchPageView(request):
-    return render(request, 'homepages/search.html')
+    
+    try:
+        person_name = request.GET['person_name']
+        missing_people = missingPerson.objects.filter(person_id=person_name)
+    except:
+        missing_people= missingPerson.objects.all()
+
+    context ={
+        'missing_people': missing_people
+    }
+
+    return render(request, 'homepages/search.html', context)
 
 
 def addpersonPageView(request):
