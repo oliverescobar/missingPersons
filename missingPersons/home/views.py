be2c689bbb190d5043f12d7c39a0305f247204b4
zolsen10 <soccerzack10@yahoo.com>
2022-12-08 18:38:10 -0700

    didnt change anything

--- missingPersons/home/views.py
+++ missingPersons/home/views.py
@@ -5,15 +5,18 @@ from home.models import missingPerson
 def indexPageView(request):
     return render(request, 'homepages/index.html')
 
+
 def HomePageView(request):
     return render(request, 'homepages/homepage.html')
+
 
 def searchPageView(request):
     return render(request, 'homepages/search.html')
 
+
 def addpersonPageView(request):
     if request.method == 'POST':
-        missingperson = missingPerson() # this is the missing persons' class we created
+        missingperson = missingPerson()  # this is the missing persons' class we created
         missingperson.date_missing = request.POST['date_missing']
         missingperson.last_name = request.POST['last_name']
         missingperson.first_name = request.POST['first_name']
@@ -22,7 +25,7 @@ def addpersonPageView(request):
         missingperson.state = request.POST['state']
         missingperson.gender = request.POST['gender']
         missingperson.race = request.POST['race']
-        
+
         missingperson.save()
 
         return HomePageView(request)
